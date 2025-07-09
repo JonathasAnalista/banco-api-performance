5c96a6f3ac02a6e7ce75c8d4f825b6ea72deab86
Jonathas Guilherme <jonathasguilhermeti@gmail.com>
2025-07-09 10:39:49 -0300

    Adicionando o arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,34 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+   iterations: 50,
+   thresholds: {
+     http_req_duration: ['p(90) < 25' ,'max < 25'],
+     http_req_failed: ['rate < 0.01']
+   }
+};
+
+export default function () {
+  // tudo que estiver aqui dentro é o nosso teste!
+  const url = 'http://localhost:3000/login';
+  const payload = JSON.stringify({
+    username: 'julio.lima',
+    senha: '123456',
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const res = http.post(url, payload, params);
+
+  check(res, {
+    'Validar que o Status é 200': (r) => r.status === 200,
+    'Validar que o Token é string': (r) => typeof(r.json().token) == 'string'
+  })
+
+  sleep(1)
+}
